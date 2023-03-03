449e0999a5192e7cfa99e4a95706561cee9f258f
Gitknol <2040861465@qq.com>
2023-03-03 17:07:22 +0800

    数据挖掘第一次实验

--- python/数据挖掘/1.py
+++ python/数据挖掘/1.py
@@ -13,7 +13,19 @@ df.dropna(inplace=True)
 #数据转化
 df.index = pd.to_datetime(df['DATE'])
 df=df['PRCP']
+#日输出图像
 ax=df.plot(kind= 'line',figsize=(15,3))
 ax.set_title('Daily Precipitation (variance = %.4f)' % (df.var()))
+plt.show()
+#月输出图像
+mouth = df.groupby(pd.Grouper(freq='M')).sum()
+ax_1=mouth.plot(kind= 'line',figsize=(15,3))
+ax_1.set_title('Monthly Precipitation (variance = %.4f)' % (mouth.var()))
+plt.show()
+
+#年输出图像
+Year = df.groupby(pd.Grouper(freq='Y')).sum()
+ax_2=Year.plot(kind= 'line',figsize=(15,3))
+ax_2.set_title('Yearly Precipitation (variance = %.4f)' % (Year.var()))
 plt.show()
 
